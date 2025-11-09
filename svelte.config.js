000e160e7579e5f5fa6d7d6d63262d181ed96969
Full Scope Media <ryan.champion2@icloud.com>
2025-11-09 15:11:53 -0500

    almsot done

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,9 +9,11 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			out: 'build',
-			precompress: false,
-			envPrefix: ''
+			// Cloudflare Pages configuration
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			}
 		})
 	}
 };
